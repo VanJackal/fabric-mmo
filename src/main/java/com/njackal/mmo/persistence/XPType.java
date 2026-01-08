49177c14b8710740f30e794f3ee61432862d4d6e
VanJackal <v@njackal.com>
2026-01-08 12:08:09 -0600

    added db ids to XPType enum

--- src/main/java/com/njackal/mmo/persistence/XPType.java
+++ src/main/java/com/njackal/mmo/persistence/XPType.java
@@ -1,22 +1,28 @@
 package com.njackal.mmo.persistence;
 
 public enum XPType {
-    Swords,
-    Archery,
-    Crossbows,
-    Tridents,
-    Spears,
-    Maces,
-    Axes,
-    Unarmed,
-    TNT,
-    Mining,
-    Excavation,
-    Woodcutting,
-    Acrobatics,
-    Alchemy,
-    Fishing,
-    Herbalism,
-    Repair,
-    Taming
+    Swords("Swords"),
+    Archery("Archery"),
+    Crossbows("Crossbows"),
+    Tridents("Tridents"),
+    Spears("Spears"),
+    Maces("Maces"),
+    Axes("Axes"),
+    Unarmed("Unarmed"),
+    TNT("TNT"),
+    Mining("Mining"),
+    Excavation("Excavation"),
+    Woodcutting("Woodcutting"),
+    Acrobatics("Acrobatics"),
+    Alchemy("Alchemy"),
+    Fishing("Fishing"),
+    Herbalism("Herbalism"),
+    Repair("Repair"),
+    Taming("Taming"),;
+
+    public final String dbId;
+
+    XPType(String dbId) {
+        this.dbId = dbId;
+    }
 }
