8414ec75c00d3b6c82ba7965bceca9f36bb60778
VanJackal <v@njackal.com>
2026-01-13 11:52:43 -0600

    canShowXpGain impl

--- src/main/java/com/njackal/mmo/logic/XPEventHandler.java
+++ src/main/java/com/njackal/mmo/logic/XPEventHandler.java
@@ -6,21 +6,24 @@ import com.njackal.mmo.persistence.DatabaseException;
 import com.njackal.mmo.persistence.MMODatabase;
 import com.njackal.mmo.persistence.XPType;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class XPEventHandler implements PlayerXPEvent {
+
+    private static final float XP_DELTA_THRESHOLD = 0.1f;//fraction of a level to show level progress
 
     private final MMODatabase database;
 
     private final List<XPGainEvent> xpGainEventHandlers;
     private final List<LevelUpEvent> levelUpEventHandlers;
 
+    private final Map<UUID, Map<XPType, Integer>> xpDeltas;
+
     public XPEventHandler(MMODatabase database) {
         this.database = database;
         xpGainEventHandlers = new LinkedList<>();
         levelUpEventHandlers = new LinkedList<>();
+        xpDeltas = new HashMap<>();
     }
 
     @Override
@@ -30,18 +33,24 @@ public class XPEventHandler implements PlayerXPEvent {
             int totalXp = database.addXp(player,type, xp);
             int level = XPMath.levelFromXp(totalXp);
 
+            incrementXPDelta(player, type, xp);
+            int delta = getXPDelta(player, type);
+
             if (didLevelUp(xp, totalXp, level)) {
                 fireLevelUpEvent(player, type, xp, totalXp, level);
-            } else if (canShowXpGain(xp, totalXp, level)){
-                fireXpGainEvent(player, type, xp, totalXp, level);
+            } else if (canShowXpGain(delta, level)){
+                fireXpGainEvent(player, type, delta, totalXp, level);
+                resetXPDelta(player, type);// reset delta since we've shown xp
             }
         } catch ( DatabaseException e ) {
             return;// fail gracefully if the database fails
         }
     }
 
-    private boolean canShowXpGain(int xp, int totalXp, int level) {
-        return true;//todo this needs a entity component that tracks xp since last levelup
+    private boolean canShowXpGain(int delta, int level) {
+        int levelDeltaXp = XPMath.xpFromLevel(level + 1) - XPMath.xpFromLevel(level);//total xp to go from level x to x+1
+        float xpThreshold = XP_DELTA_THRESHOLD * (float)levelDeltaXp; // threshold amount
+        return xpThreshold <= delta; // is change in xp greater than threshold change
     }
 
     private boolean didLevelUp(int xpGain, int totalXp, int level) {
@@ -67,6 +76,28 @@ public class XPEventHandler implements PlayerXPEvent {
     private void fireLevelUpEvent(UUID player, XPType type, int xpGain, int xpTotal, int level) {
         for (LevelUpEvent levelUpEvent : levelUpEventHandlers) {
             levelUpEvent.levelUp(player, type, xpGain, xpTotal, level);
+        }
+    }
+
+    private int getXPDelta(UUID player, XPType type) {
+        tryInitXPDelta(player);
+        return xpDeltas.get(player).getOrDefault(type,0);
+    }
+
+    private void resetXPDelta(UUID player, XPType type) {
+        tryInitXPDelta(player);
+        xpDeltas.get(player).put(type,0);
+    }
+
+    private void incrementXPDelta(UUID player, XPType type, int delta) {
+        tryInitXPDelta(player);
+        Map<XPType, Integer> xpDelta = xpDeltas.get(player);
+        xpDelta.put(type,xpDelta.getOrDefault(type,0) + delta);
+    }
+
+    private void tryInitXPDelta(UUID player) {
+        if (!xpDeltas.containsKey(player)) {
+            xpDeltas.put(player, new HashMap<>());
         }
     }
 }
