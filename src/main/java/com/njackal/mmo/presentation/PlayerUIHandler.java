0ba28ad9eda0f01b0a2ff7e15dda393162292e01
VanJackal <v@njackal.com>
2026-01-16 11:07:30 -0600

    actionbar and chat levelup notif modes

--- src/main/java/com/njackal/mmo/presentation/PlayerUIHandler.java
+++ src/main/java/com/njackal/mmo/presentation/PlayerUIHandler.java
@@ -7,8 +7,10 @@ import com.njackal.mmo.logic.XPGainEvent;
 import com.njackal.mmo.persistence.XPType;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.bossevents.CustomBossEvent;
@@ -49,6 +51,8 @@ public class PlayerUIHandler implements LevelUpEvent, XPGainEvent {
         if (serverPlayer != null) {
             switch (configHandler.getNotificationMode(player)) {
                 case Title -> levelUpTitle(type.dbId, level, serverPlayer);
+                case Actionbar -> levelUpActionbar(type.dbId, level, serverPlayer);
+                case Chat -> levelUpChat(type.dbId, level, serverPlayer);
             }
 
             xpGained(player, type, 1,1);// show a full xp bar on level up
@@ -124,6 +128,21 @@ public class PlayerUIHandler implements LevelUpEvent, XPGainEvent {
         ));
         serverPlayer.connection.send(new ClientboundSetTitleTextPacket(
                 Component.literal("Level Up!").withStyle(ChatFormatting.GOLD)
+        ));
+    }
+
+    private void levelUpChat(String type, int level, ServerPlayer serverPlayer) {
+        serverPlayer.connection.send(
+                new ClientboundSystemChatPacket(
+                        Component.literal(String.format("§f%s§r leveled up to level §f%d", type, level)).withStyle(ChatFormatting.GOLD),
+                        false
+                )
+        );
+    }
+
+    private void levelUpActionbar(String type, int level, ServerPlayer serverPlayer) {
+        serverPlayer.connection.send(new ClientboundSetActionBarTextPacket(
+                Component.literal(String.format("§f%s§r leveled up to level §f%d", type, level)).withStyle(ChatFormatting.GOLD)
         ));
     }
 }
